0fb7bad821022e4de29484637da45f90dc69494d
subbbbbaru <saa2410@mail.ru>
2022-03-15 18:01:13 +1000

    I need Fix File exist

--- app/src/main/java/com/example/sampleserver/MainActivity.java
+++ app/src/main/java/com/example/sampleserver/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.sampleserver;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -14,22 +22,42 @@ import com.sun.net.httpserver.HttpServer;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
+
+import sun.misc.Resource;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean serverUp = false;
     private HttpServer httpServer;
 
+    String msgLog = "";
+    String fileName = "";
+    String filePath = "";
+    int PORT = 5055;
+
     TextView serverTextView;
     Button serverButton;
+    ScrollView scrollView;
+    TextView msg;
 
 
     // https://www.youtube.com/watch?v=1JgV3eNs-_4
@@ -42,15 +70,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int PORT = 5055;
+        fileName = "testFile.txt";
+        filePath = "testDir";
 
-        serverTextView = findViewById(R.id.serverTextView);
-        serverButton = findViewById(R.id.serverButton);
+
+        serverTextView = (TextView) findViewById(R.id.serverTextView);
+        serverButton = (Button) findViewById(R.id.serverButton);
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
+        msg = (TextView) findViewById(R.id.msg);
+
+
 
         serverButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!serverUp){
+                if(!serverUp && isExternalStorageAvaibleForRW()){
                     startServer(PORT);
                     serverUp = true;
                 }
@@ -61,6 +95,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private String getIpAddress(){
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()){
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()){
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if(inetAddress.isSiteLocalAddress()){
+                        ip += "Site Local Address:\n"
+                                + inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e){
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+        return ip;
     }
 
     public static String streamToString(InputStream inputStream){
@@ -74,11 +134,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public static void sendResponse(HttpExchange httpExchange, String responseText) throws IOException {
-        httpExchange.sendResponseHeaders(200, responseText.length());
+    public void sendResponse(HttpExchange httpExchange, String responseText) throws IOException {
+        byte[] bytes = responseText.getBytes(StandardCharsets.UTF_8);
+        httpExchange.sendResponseHeaders(200, bytes.length);
+        //serverTextView.setText(httpExchange.getLocalAddress().toString());
         OutputStream os = httpExchange.getResponseBody();
-        os.write(responseText.getBytes(Charset.defaultCharset()));
-        os.flush();
+        os.write(bytes);
+        //os.flush();
         os.close();
     }
 
@@ -88,45 +150,50 @@ public class MainActivity extends AppCompatActivity {
 
        try {
            httpServer = HttpServer.create(new InetSocketAddress(port), 0);
-           httpServer.setExecutor(Executors.newCachedThreadPool());
+           httpServer.setExecutor(null);
 
            httpServer.createContext("/", new HttpHandler() {
                @Override
                public void handle(HttpExchange httpExchange) throws IOException {
+                   //serverTextView.setText("Server is running..." + httpExchange.getLocalAddress().toString());
                    if("GET".equals(httpExchange.getRequestMethod())){
-                       sendResponse(httpExchange,"Welcome to my server");
+                       sendResponse(httpExchange, "Hello, Andrey!");
+                   } else {
+
+                       sendResponse(httpExchange, "Buy, Andrey!");
                    }
                }
            });
 
-           //httpServer.createContext("/", new rootHandler());
-           //httpServer.createContext("/index", new rootHandler());
+           httpServer.createContext("/messages", new messageHandler());
+           httpServer.start();
 
-           //httpServer.createContext("/messages", new messageHandler());
-           serverTextView.setText("Server is running...");
+
+           serverTextView.setText(getIpAddress() + ":" + port + "\n");
            serverButton.setText("Stop Server");
+           DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy 'at' HH:mm:ss");
+           String date = dateFormat.format(Calendar.getInstance().getTime());
+           msgLog += date + " : Server Start\n";
 
+           viewScrollView(msgLog);
+           File myExtStor = new File(getExternalFilesDir(filePath), fileName);
+           FileOutputStream fos = null;
+           fos = new FileOutputStream(myExtStor);
+           fos.write(msgLog.getBytes(StandardCharsets.UTF_8));
 
-       } catch (IOException e) {
+       } catch (FileNotFoundException e){
+           e.printStackTrace();
+       }
+       catch (IOException e) {
            e.printStackTrace();
        }
 
     }
 
-    public class rootHandler implements HttpHandler{
+
+
+    public class messageHandler implements HttpHandler{
         @Override
-        public void handle(HttpExchange httpExchange) throws IOException {
-            if("GET".equals(httpExchange.getRequestMethod())){
-                sendResponse(httpExchange,"Welcome to my server");
-            }
-        }
-
-
-    }
-
-    /*public class messageHandler implements HttpHandler{
-        @Override
-
         public void handle(HttpExchange httpExchange) throws IOException {
             if("GET".equals(httpExchange.getRequestMethod())){
                 sendResponse(httpExchange, "Would be all messages stringified json");
@@ -134,27 +201,104 @@ public class MainActivity extends AppCompatActivity {
                 InputStream inputStream = httpExchange.getRequestBody();
                 String requestBody = streamToString(inputStream);
                 JSONObject jsonBody = null;
+
+
                 try {
                     jsonBody = new JSONObject(requestBody);
+                    if (!jsonBody.get("Phone").toString().isEmpty() && !jsonBody.get("Text").toString().isEmpty())
+                    {
+                        String goodRequest = "{ \n" +
+                                "    \"StatusOnPhone\": \"Yes\", \n" +
+                                "}";
+                        String Phone = jsonBody.get("Phone").toString();
+                        String Text = jsonBody.get("Text").toString();
+
+                       // Telephony.SMS(Phone, Text);
+
+                        sendResponse(httpExchange, goodRequest.toString());
+                    } else {
+                        String badRequest = "{ \n" +
+                                "    \"StatusOnPhone\": \"No\", \n" +
+                                "}";
+
+                        sendResponse(httpExchange, badRequest.toString());
+                    }
+
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    sendResponse(httpExchange, e.getMessage());
                 }
 
-                sendResponse(httpExchange, jsonBody.toString());
             }
 
 
         }
 
-    }*/
+    }
 
     private void stopServer() {
         if(httpServer != null){
-            httpServer.stop(0);
-            serverTextView.setText("Server is down");
-            serverButton.setText("Start Server");
+            try {
+                httpServer.stop(0);
+                serverTextView.setText("Server is down");
+                serverButton.setText("Start Server");
+
+
+
+                DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy 'at' HH:mm:ss");
+                String date = dateFormat.format(Calendar.getInstance().getTime());
+                msgLog += date + " : Server Stop\n\n";
+
+
+
+
+                viewScrollView(msgLog);
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+
         }
 
+    }
+    private void viewScrollView(String data){
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                writeData(data);
+                msg.setText(data, TextView.BufferType.SPANNABLE);
+
+            }
+        });
+    }
+
+    private boolean isExternalStorageAvaibleForRW(){
+        String exStorageState = Environment.getExternalStorageState();
+        if(exStorageState.equals(Environment.MEDIA_MOUNTED)){
+            return true;
+        }
+        return false;
+    }
+
+    private void writeData(String data) {
+        try {
+            File myExtStor = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), fileName);
+            FileOutputStream fos = null;
+            fos = new FileOutputStream(myExtStor);
+            if(myExtStor.exists()){
+                fos.
+                fos.write(data.getBytes(StandardCharsets.UTF_8));
+            }
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
---
+++ app/src/main/java/com/example/sampleserver/Telephony.java
@@ -0,0 +1,62 @@
+package com.example.sampleserver;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.telephony.SmsManager;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+public class Telephony {
+
+    public static Context context;
+    public static String[] mSet;
+
+    public static void Telephony (Context context1) {
+        context = context1;
+    }
+
+    public static void answerPhoneHeadsethook() {
+        // «Нажимаем» и «отпускаем» кнопку на гарнитуре
+        Intent buttonDown = new Intent( Intent.ACTION_MEDIA_BUTTON);
+        buttonDown.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent( KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_HEADSETHOOK));
+        context.sendOrderedBroadcast(buttonDown, "android.permission.CALL_PRIVILEGED");
+
+        Intent buttonUp = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        buttonUp.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HEADSETHOOK));
+        context.sendOrderedBroadcast(buttonUp, "android.permission.CALL_PRIVILEGED");
+    }
+
+    public static void SMS(String tel, String text) {
+
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage( tel, null, text, null, null );
+            Toast.makeText( context,
+                    "SMS отправлено!", Toast.LENGTH_LONG ).show();
+            }
+            catch (Exception e){
+                    Toast.makeText(context,
+                            "SMS не отправлено, попытайтесь еще! " + e.toString(), Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+            }
+
+    }
+
+    public static void Call(String tel) {
+//        Intent call = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + tel));
+//        context.startActivity(call);
+        Intent callIntent = new Intent(Intent.ACTION_CALL); //use ACTION_CALL class
+        callIntent.setData( Uri.parse("tel:" + tel));    //this is the phone number calling
+        //check permission
+        //If the device is running Android 6.0 (API level 23) and the app's targetSdkVersion is 23 or higher,
+        //the system asks the user to grant approval.
+        try{
+                context.startActivity(callIntent);  //call activity and make phone call
+            }
+            catch (android.content.ActivityNotFoundException ex){
+                Toast.makeText(context, "did not work" + "\n" + ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+    }
+
+}
